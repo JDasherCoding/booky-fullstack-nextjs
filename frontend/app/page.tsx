c531f2022323dff400068e41d3fa52ed717ba1cd
James D <66693959+James-Da@users.noreply.github.com>
2024-07-09 20:53:40 +0200

    fullstack hotel booking app

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Rooms from "@/components/Rooms";
+
+export default function Home() {
+	return (
+		<main>
+			<Rooms />
+		</main>
+	);
+}
---
+++ frontend/app/room/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomDetails = () => {
+	return <div>Room Details</div>;
+};
+
+export default RoomDetails;
